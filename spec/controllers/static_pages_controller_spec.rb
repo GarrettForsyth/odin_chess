305c5e0dc1ec864f4cc84627062946dbaf748fd2
Garrett Forsyth <gtforsyth91@gmail.com>
2018-04-26 09:54:00 -0400

    mm

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -16,16 +16,17 @@ RSpec.describe StaticPagesController, type: :controller do
   end
 
   describe 'GET lobby' do
-    before do
+
+    it 'renders the sign in page when user is not logged in' do
       get :lobby
+      expect(response).to redirect_to(new_user_session_path)
     end
 
-    it 'renders the lobby page' do
+    it 'renders the lobby if the user is signed in'  do
+      user = FactoryBot.create(:user)
+      sign_in user
+      get :lobby
       expect(response).to render_template('lobby')
-    end
-
-    it 'renders the application layout' do
-      expect(response).to render_template('layouts/application')
     end
   end
 end
--- spec/views/partials/global_nav.html.erb_spec.rb
+++ spec/views/partials/global_nav.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'layouts/_global_nav.html.erb' do
-  it 'contains links' do
+  it 'contains links if user is signed out' do
     render
     expect(rendered).to have_xpath("//a[@href='#{root_path}']",
                                    text: 'Odin Chess',
@@ -13,4 +13,21 @@ describe 'layouts/_global_nav.html.erb' do
                                    text: 'Sign Up',
                                    count: '1')
   end
+
+  it 'contains links if user is signed in' do
+    user = FactoryBot.create(:user)
+    sign_in user
+
+    render
+    expect(rendered).to have_xpath("//a[@href='#{root_path}']",
+                                   text: 'Odin Chess',
+                                   count: 1)
+    expect(rendered).to have_xpath("//a[@href='#{destroy_user_session_path}']",
+                                   text: 'Logout',
+                                   count: '1')
+    expect(rendered).to have_xpath("//a[@href='#{new_user_registration_path}']",
+                                   text: 'Sign Up',
+                                   count: '1')
+  end
+
 end
