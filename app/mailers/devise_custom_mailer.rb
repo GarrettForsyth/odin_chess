0dc9abe10ca5e2e6d2471763f9ccaca8a1907904
Garrett Forsyth <gtforsyth91@gmail.com>
2018-04-26 15:39:11 -0400

    changed image tag call

--- app/mailers/devise_custom_mailer.rb
+++ app/mailers/devise_custom_mailer.rb
@@ -6,7 +6,7 @@ class DeviseCustomMailer < Devise::Mailer
   add_template_helper ApplicationHelper
 
   def confirmation_instructions(record, token, opts={})
-    attachments.inline["horse.svg"] = File.read("#{Rails.root.to_s.gsub('/app','')}/app/assets/images/horse.svg")
+    attachments.inline['horse.svg'] = File.read("#{Rails.root}/app/assets/images/horse.svg")
     super
   end
 
