c2903d9e0047a565320ad17844500d07c350d812
Garrett Forsyth <gtforsyth91@gmail.com>
2018-04-26 16:12:15 -0400

    test image

--- app/mailers/devise_custom_mailer.rb
+++ app/mailers/devise_custom_mailer.rb
@@ -2,25 +2,22 @@ class DeviseCustomMailer < Devise::Mailer
   helper :application
   default template_path: 'devise/mailer'
   layout 'mailer'
-  add_template_helper EmailHelper
   add_template_helper ApplicationHelper
 
-  def confirmation_instructions(record, token, opts={})
+  def confirmation_instructions(record, token, opts = {})
     attachments.inline['horse.svg'] = File.read("#{Rails.root}/app/assets/images/horse.svg")
     super
   end
 
-  def reset_password_instructions(record, token, opts={})
+  def reset_password_instructions(record, token, opts = {})
     super
   end
 
-  def password_change(record, token, opts={})
+  def password_change(record, token, opts = {})
     super
   end
 
-  def email_changed(record, token, opts={})
+  def email_changed(record, token, opts = {})
     super
   end
-
-
 end
