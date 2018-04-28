284a6bfc20a143e11e582ba4a273b8fe87e1b9fb
Garrett Forsyth <gtforsyth91@gmail.com>
2018-04-28 18:29:10 -0400

    added login by handle or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < ApplicationRecord
          :validatable,
          :confirmable
 
+  # Virtual attribute for authenticating by either handle or email
+  # This is in addition to a real persisted field like 'handle'
+  attr_accessor :login
+
   VALID_HANDLE_REGEX = /\A[a-zA-Z0-9\-_]+\z/ # Only letters letters and numbers
   validates :handle, presence: true,
                      length: { maximum: 20 },
@@ -18,4 +22,13 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, length: { maximum: 256 }, format: { with: VALID_EMAIL_REGEX }
 
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(['lower(handle) = :value OR lower(email) = :value', { value: login.downcase }]).first
+    elsif conditions.key?(:handle) || conditions.key?(:email)
+      conditions[:email].downcase! if conditions[:email] 
+      where(conditions.to_h).first
+    end
+end
 end
